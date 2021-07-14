1e6ae884b9b86541a5d3a65903c9b6c5cf3629c7
Shane Fitzpatrick <fitzpatrick.shane@gmail.com>
2021-07-14 02:33:40 +0100

    Fix upcoming movies page and Change upcoming movies page to use caching

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -7,8 +7,8 @@ export const getMovies = async () => {
   }
   return response.json();
 };
-  
-export const getMovie = async ( args ) => {
+
+export const getMovie = async (args) => {
   // console.log(args)
   // eslint-disable-next-line no-unused-vars
   const [prefix, { id }] = args.queryKey;
@@ -20,46 +20,48 @@ export const getMovie = async ( args ) => {
   }
   return response.json();
 };
-  
-  export const getGenres = async () => {
-    const response = await  fetch(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
-        process.env.REACT_APP_TMDB_KEY +
-        "&language=en-US"
-    )
-    if (!response.ok) {
-      throw new Error(response.json().message);
-    }
-    return response.json();
-  };
-  
-  export const getMovieImages = async ({queryKey}) => {
-    // eslint-disable-next-line no-unused-vars
-    const [prefix, { id }] = queryKey;
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-    if (!response.ok) {
-      throw new Error(response.json().message);
-    }
-    return response.json();
-  };
 
-  export const getMovieReviews = (id) => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        // console.log(json.results);
-        return json.results;
-      });
-  };
+export const getGenres = async () => {
+  const response = await fetch(
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+    process.env.REACT_APP_TMDB_KEY +
+    "&language=en-US"
+  )
+  if (!response.ok) {
+    throw new Error(response.json().message);
+  }
+  return response.json();
+};
 
-  export const getUpcoming = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then(res => res.json())
-      .then(json => json.results);
-  };
+export const getMovieImages = async ({ queryKey }) => {
+  // eslint-disable-next-line no-unused-vars
+  const [prefix, { id }] = queryKey;
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  )
+  if (!response.ok) {
+    throw new Error(response.json().message);
+  }
+  return response.json();
+};
+
+export const getMovieReviews = (id) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      // console.log(json.results);
+      return json.results;
+    });
+};
+
+export const getUpcoming = async () => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+  ); if (!response.ok) {
+    throw new Error(response.json().message);
+  }
+  return response.json();
+};
+
--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,31 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcoming } from "../api/tmdb-api";
+import { useQuery } from 'react-query'
+import Spinner from '../components/spinner'
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites';
 
 const UpCoomingMoviesPage = (props) => {
-  const [movies, setMovies] = useState([]);
+  const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcoming)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
+  // const addToFavorites = (movieId) => true 
 
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
-
-  useEffect(() => {
-    getUpcoming().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <PageTemplate
       title='Upcoming Movies'
       movies={movies}
-      selectFavorite={addToFavorites}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
     />
   );
 };
